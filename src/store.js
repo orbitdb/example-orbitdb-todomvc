20fa142761ca96ed17f03d2a303b1dd12e1b77ec
anders <andersb@tuta.io>
2020-12-07 20:53:43 -0600

    add webrtc servers

--- src/store.js
+++ src/store.js
@@ -14,6 +14,9 @@ const ipfsConfig = {
         // '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
         // WebRTC:
         // '/dns4/star-signal.cloud.ipfs.team/wss/p2p-webrtc-star',
+        '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
+        '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
+        '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/',
         // Use local signal server
         // '/ip4/0.0.0.0/tcp/9090/wss/p2p-webrtc-star',
       ]
